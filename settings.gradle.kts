688851bcb42fb376050aa74136943060cebf390a
Yaroslav Zonov <animationannihilationhd@gmail.com>
2026-04-21 15:28:42 +0300

    fix: correct dependencyResolutionManagement in settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,7 +6,9 @@ pluginManagement {
     }
 }
 
-dependencyResolution {
+@Suppress("UnstableApiUsage")
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
